7b93d61670379aca8dda1fafbacc84edc040c8a0
ruslan <mstrbsc@gmail.com>
2019-07-17 23:15:22 +0300

    -

---
+++ bmp2nc.c
@@ -0,0 +1,89 @@
+/***********************
+
+
+
+************************/
+
+
+uint8_t img_stat, nc_stat;
+uint32_t img_x, img_y, img_dpi_x, img_dpi_y;
+
+uint8_t num_format; // 0x32 >>> 002.27
+
+int32_t offset_x, offset_y;    /// zero point in image
+
+char img_fl[256], nc_fl[256], tmp_img_fl[256], tmp_nc_fl[256];
+char n_nc[256], tmp_n1_nc[256], tmp_n2_nc[256], tmp_n3_nc[256], tmp_n4_nc[256];
+
+
+
+
+int init_system (void)
+{
+  
+  
+  
+  
+  
+  
+  
+  
+  
+}
+
+
+int open_img ( char *imgfl)
+{
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+}
+
+
+int open_nc (char *ncfl)
+{
+  
+  
+  
+ 
+  
+  
+  
+  
+  
+  
+  
+  
+}
+
+int open_tmp_fl ( void )
+{
+  
+  
+  
+  
+  
+  
+  
+  
+}
+
+int mk_bs_nc ( void )
+{
+  
+  
+  
+  
+  
+}
+
+
